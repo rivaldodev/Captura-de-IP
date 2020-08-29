2760b80626fed166f623c6b5cbee1517960adbf1
Rivaldo Freitas de Carvalho <70446123+rivaldodev@users.noreply.github.com>
2020-08-29 14:18:28 -0300

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,23 @@
+<?PHP
+function capIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+    return $ip;
+}
+$user_ip = capIP();
+echo $user_ip; // Saída endereço de IP [Ex: 177.87.193.134]
+?>
